91ee4896ff4dc996710688e90873b9b30d728212
ellen <ellenhalvardsson@gmail.com>
2024-05-22 13:09:31 +0200

    added agent

---
+++ src/main/java/org/example/Agent.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+
+import java.io.IOException;
+
+public class Agent {
+    // Note that the 'agentmain' method has a String parameter instead of a standard main method with an array parameter.
+    public static void agentmain(String agentargs) throws InterruptedException {
+        while (!Thread.interrupted()) {
+            Thread.sleep(2_000);
+            System.out.println("Hello Agent!");
+        }
+    }
+
+    // Here we use VirtualMachine API to merge two programs/JAR-files to one JVM process.
+    public static void main(String[] args) throws AgentLoadException, IOException, AgentInitializationException, AttachNotSupportedException {
+        VirtualMachine vm = VirtualMachine.attach("279892");
+        try {
+            vm.loadAgent("C:/Users/ellen/IdeaProjects/agent-demo/target/agent-demo-1.0-SNAPSHOT.jar");
+        } finally {
+            vm.detach();
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Here we get the current JVM process id and print it along with a message.
+    public static void main(String[] args) throws InterruptedException{
+        long pid = ProcessHandle.current().pid();
+        System.out.println("PID :" + pid);
+        while (!Thread.interrupted()) {
+            Thread.sleep(2_000);
+            System.out.println("Hello world!");
+        }
     }
 }
